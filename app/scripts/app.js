3a8e693b22b767a51ea523c13b91725cb51dbe62
unknown <IvanP@CO-YAR-WS160.firmglobal.com>
2015-11-18 21:14:26 +0300

    kpi-tile updated, now it calculates height automatically and can receive info  inside kpi-tile content area with #info id instead of info attribute

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,7 @@ The complete set of contributors may be found at http://polymer.github.io/CONTRI
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
-
+/*
 (function(document) {
   'use strict';
 
@@ -64,4 +64,65 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     }
   };
 
-})(document);
+})(document);*/
+
+// 4. Conditionally load the webcomponents polyfill if needed by the browser.
+// This feature detect will need to change over time as browsers implement
+// different features.
+var webComponentsSupported = ('registerElement' in document
+&& 'import' in document.createElement('link')
+&& 'content' in document.createElement('template'));
+
+if (!webComponentsSupported) {
+  console.log('webComponents not supported');
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = '/isa/BDJPFRDMEYBPBKLVADAYFQCDAVIOEQJR/Polymer/webcomponents-lite.js';
+  script.onload = finishLazyLoading;
+  document.head.appendChild(script);
+} else {
+  console.log('webComponents supported');
+  finishLazyLoading();
+}
+
+function finishLazyLoading() {
+  // (Optional) Use native Shadow DOM if it's available in the browser.
+  window.Polymer = window.Polymer || {dom: 'shadow'};
+  console.log(window.Polymer);
+  // 6. Fade splash screen, then remove.
+  var onImportLoaded = function() {
+    console.log('on import loaded');
+    var loadEl = document.getElementById('splash');
+    loadEl.addEventListener('transitionend', function(){
+      loadEl.style.display='none';
+      loadEl.parentNode.removeChild(loadEl);
+    });
+
+    document.body.classList.remove('loading');
+
+    // App is visible and ready to load some data!
+  };
+
+  var link = document.querySelector('#bundle');
+
+  // 5. Go if the async Import loaded quickly. Otherwise wait for it.
+  // crbug.com/504944 - readyState never goes to complete until Chrome 46.
+  // crbug.com/505279 - Resource Timing API is not available until Chrome 46.
+  if (link.import && link.import.readyState === 'complete') {
+    console.log('link import complete');
+    if(!webComponentsSupported){
+      window.addEventListener('WebComponentsReady', onImportLoaded)
+    } else {
+      onImportLoaded();
+    }
+
+  } else {
+    console.log('listen to LOAD');
+    if(!webComponentsSupported){
+      window.addEventListener('WebComponentsReady', onImportLoaded)
+    } else {
+      link.addEventListener('load', onImportLoaded);
+    }
+
+  }
+}
